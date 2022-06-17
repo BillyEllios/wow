e10747fcf6f5a7a67f8acdb84c64e2863c9a7f14
Paul <paul.boyer170595@gmail.com>
2022-06-17 15:21:11 +0200

    Update

--- migrations/Version20220617132046.php
+++ migrations/Version20220617132046.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220616132342 extends AbstractMigration
+final class Version20220617132046 extends AbstractMigration
 {
     public function getDescription(): string
     {
